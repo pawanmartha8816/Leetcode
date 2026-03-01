3866be6143a677430c48f4c118b2fc3e2f3946f6
pawanmartha8816 <pawanmartha16@gmail.com>
2026-03-01 09:13:20 +0530

    Time: 2106 ms (25.12%), Space: 42.3 MB (44.6%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean checkPerfectNumber(int n) {
+        int s=0;
+        for(int i=1;i<n;i++){
+            if(n%i==0) s+=i;
+        }
+        //System.out.print(s);
+        if(s==n) return true;
+        else return false;
+    }
+}
